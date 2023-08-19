5cf79383cd535ca6fe7000ef59aeac28545a8601
Adarsh Maurya <adarshmaurya0022@gmail.com>
2023-08-19 21:16:03 +0530

    more changes

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,40 @@
 
+
+function getImageThumbnail(path, img, title) {
+    const filePath = path; // Replace with your desired file path
+    fetch(filePath)
+        .then(response => response.blob())
+        .then(blob => {
+            jsmediatags.read(blob, {
+                onSuccess: function (tag) {
+                    if (tag && tag.tags.picture) {
+                        const picture = tag.tags.picture;
+                        const base64String = btoa(
+                            new Uint8Array(picture.data).reduce((data, byte) => data + String.fromCharCode(byte), '')
+                        );
+                        const imageSrc = `data:${picture.format};base64,${base64String}`;
+                        img.src = imageSrc;
+                        const artistName = tag.tags.artist || "Unknown Artist"; // Get the artist name or use "Unknown Artist" if not available
+                        const songName = tag.tags.title || "Unknown Song";
+                        title.innerHTML = `${songName.split('(')[0]} <br/> <span class=\"subtitle\">${artistName}</span>`
+                    } else {
+                        const artistName = tag.tags.artist || "Unknown Artist"; // Get the artist name or use "Unknown Artist" if not available
+                        const songName = tag.tags.title || "Unknown Song";
+                        title.innerHTML = `${songName.split('(')[0]} <br/> <span class=\"subtitle\">${artistName}</span>`
+                        img.src = './otherImages/music-icon.svg'
+                    }
+                },
+                onError: function (error) {
+                    console.log("Error reading metadata:", error);
+                }
+            });
+        })
+        .catch(error => {
+            console.log("Error fetching the file:", error);
+        });
+
+}
+
 const currentStart = document.getElementById("currentStart");
 const currentEnd = document.getElementById("currentEnd");
 const music = new Audio();
@@ -22,109 +58,6 @@ function customAlertHide() {
     document.querySelector(".customAlert .alert").style.transform = "translateY(-105%)";
 }
 
-// const songs = [
-//     {
-//         id: 1,
-//         songName: `On My Way <br> <span class="subtitle">Alon Walker</span>`,
-//         poster: './img/1.jpg'
-//     },
-//     {
-//         id: 2,
-//         songName: `Alon Walker-Fade <br> <span class="subtitle">Alon Walker</span>`,
-//         poster: './img/2.jpg'
-//     },
-//     {
-//         id: 3,
-//         songName: `Cartoon - On & On <br> <span class="subtitle">Daniel Levi</span>`,
-//         poster: './img/3.jpg'
-//     },
-//     {
-//         id: 4,
-//         songName: `Warriyo - Mortals <br> <span class="subtitle">Mortals</span>`,
-//         poster: './img/4.jpg'
-//     },
-//     {
-//         id: 5,
-//         songName: `Ertugrul Gazi <br> <span class="subtitle">Ertugrul</span>`,
-//         poster: './img/5.jpg'
-//     },
-//     {
-//         id: 6,
-//         songName: `Electronic Music <br> <span class="subtitle">Electro</span>`,
-//         poster: './img/6.jpg'
-//     },
-//     {
-//         id: 7,
-//         songName: `Agar Tum Sath Ho<br> <span class="subtitle">Tamashaa</span>`,
-//         poster: './img/7.jpg'
-//     },
-//     {
-//         id: 8,
-//         songName: `Suna Hai <br> <span class="subtitle">Jubin Nautial</span>`,
-//         poster: './img/8.jpg'
-//     },
-//     {
-//         id: 9,
-//         songName: `Dilbar <br> <span class="subtitle">Neha kakkar, Dhvani Bhanushali, Ikka</span>`,
-//         poster: './img/9.jpg'
-//     },
-//     {
-//         id: 10,
-//         songName: `Dunia <br> <span class="subtitle">Abhijit Vaghani, Akhil, Dhvani Bhanushali, Bob</span>`,
-//         poster: './img/10.jpg'
-//     },
-//     {
-//         id: 11,
-//         songName: `Lagdi Lahore Di <br> <span class="subtitle">Guru Randhawa, Tulsi kumar </span>`,
-//         poster: './img/11.jpg'
-//     },
-//     {
-//         id: 12,
-//         songName: `Putt Jatt De <br> <span class="subtitle">Diljit Dosanjh, Kaater</span>`,
-//         poster: './img/12.jpg'
-//     },
-//     {
-//         id: 13,
-//         songName: `Baarishein <br> <span class="subtitle">Atif Aslam</span>`,
-//         poster: './img/13.jpg'
-//     },
-//     {
-//         id: 14,
-//         songName: `Vaste <br> <span class="subtitle">Dhvani Bhanushali, Nikhil D Souza</span>`,
-//         poster: './img/14.jpg'
-//     },
-//     {
-//         id: 15,
-//         songName: `Lut Gay <br> <span class="subtitle">Dhvani Bhanushali, Nikhil D Souza</span>`,
-//         poster: './img/15.jpg'
-//     },
-// ]
-// Dynamically modifing all divs that contain music
-// songs.forEach((element, i) => {
-//     if (i < 7) {
-//         let temp = (i < 9) ? "0" + (i + 1) : (i + 1);
-//         document.querySelector(".menu-songs ul").innerHTML += `
-//                 <li class="song-list list" onmouseover="this.classList.add('hover');" onmouseout="this.classList.remove('hover');">
-//                     <div>
-//                         <span>${temp}</span>
-//                         <img src="${element.poster}" class="" alt="">
-//                         <h5 class="title">${element.songName}</h5>
-//                     </div>
-//                     <i class="bi playbutton bi-play-circle-fill" id="${element.id}"></i>
-//                 </li>
-//         `;
-//     } else {
-//         document.querySelector(".items ul").innerHTML += `
-//             <li class="song-list list">
-//                 <div class="song">
-//                     <img src="${element.poster}" alt="">
-//                     <i class="bi playbutton bi-play-circle-fill" id="${i + 1}"></i>
-//                 </div>
-//                 <h5 class="title">${element.songName}</h5>
-//             </li>
-//         `
-//     }
-// });
 // we pause all the buttons when another music is played
 function pauseAllBtns() {
     Array.from(document.getElementsByClassName("bi-pause-circle-fill")).forEach(e => {
@@ -137,23 +70,72 @@ const progressBar = document.getElementById("seek");
 const seekBar = document.getElementById("bar1");
 
 // changing master play content dynamically when music is played
-function changeMasterPlay(i) {
+function changeMasterPlay(i, id) {
+    const searchSongById = (id) => {
+        const song = songs.find(item => item.id === id);
+        return song ? song.songName : "Song not found";
+    };
+    const searchedSong = searchSongById(parseInt(i));
+    console.log(i + " " + id);
     document.querySelector("footer .controles").innerHTML = `
-<div class="wave ">
-    <div class="wave1"></div>
-    <div class="wave1"></div>
-    <div class="wave1"></div>
-</div>
-<img src="./img/${songs[i].id}.jpg" id="poster" alt="">
-<h5 class="title">${songs[i].songName}</span></h5>
-<div class="icons">
-    <i class="bi bi-shuffle"></i>
-    <i class="bi bi-skip-start-fill" onclick="goPrev(this)"></i>
-    <i class="bi bi-play-fill  play-and-pause" onclick="playmasterplay(this)" id="0-${songs[i].id}"></i>
-    <i class="bi bi-skip-end-fill" onclick="goNext(this)"></i>
-</div>
+        <div class="wave ">
+            <div class="wave1"></div>
+            <div class="wave1"></div>
+            <div class="wave1"></div>
+        </div>
+        <img src="" id="poster" alt="">
+
+        <h5 class="title" id="title-${id}&"></h5>
+        <div class="icons">
+            <i class="bi bi-shuffle"></i>
+            <i class="bi bi-skip-start-fill" onclick="goPrev(this)"></i>
+            <i class="bi bi-play-fill  play-and-pause" data-custom-value="${i} ${id}" onclick="playmasterplay(this)" id="0-${id}"></i>
+            <i class="bi bi-skip-end-fill" onclick="goNext(this)" ></i>
+        </div>
 `;
     document.querySelector("footer .tracker").style.pointerEvents = "all";
+    getImageThumbnail(`../audio/${id}.mp3`, document.getElementById("poster"), document.getElementById(`title-${id}&`))
+}
+// function to go previous song
+function goPrev(e) {
+    let index = document.querySelector(".play-and-pause").dataset.customValue.split(" ")[0];
+    let songid = document.querySelector(".play-and-pause").dataset.customValue.split(" ")[1];
+    pauseAllBtns();
+    deactiveMenuSongs();
+    index = parseInt(index) - 1;
+    if (index < 0) {
+        customAlertShow("This is the first song that you are listening!")
+    } else {
+        songid = parseInt(songs[index].id);
+        changeMasterPlay(index, songid);
+        music.src = `./audio/${songid}.mp3`;
+        buffering();
+        music.play();
+        document.querySelector(".icons .play-and-pause").classList.add("bi-pause-fill")
+        document.querySelector(".icons .play-and-pause").classList.remove("bi-play-fill")
+        document.querySelector("footer img").classList.add("spining");
+        document.querySelector("footer .wave").classList.add("active1");
+    }
+}
+
+// function to go next song
+function goNext(e) {
+    let index = document.querySelector(".play-and-pause").dataset.customValue.split(" ")[0];
+    let songid = document.querySelector(".play-and-pause").dataset.customValue.split(" ")[1];
+    index = parseInt(index) + 1;
+    if (index >= songs.length) {
+        customAlertShow("This is the last song that you are listening!")
+    } else {
+        songid = parseInt(songs[index].id);
+        changeMasterPlay(index, songid);
+        music.src = `./audio/${songid}.mp3`;
+        buffering();
+        music.play();
+        document.querySelector(".icons .play-and-pause").classList.add("bi-pause-fill")
+        document.querySelector(".icons .play-and-pause").classList.remove("bi-play-fill")
+        document.querySelector("footer img").classList.add("spining");
+        document.querySelector("footer .wave").classList.add("active1");
+    }
 }
 // this function used to show buffering of the music
 function buffering() {
@@ -179,10 +161,17 @@ function buffering() {
     music.load();
 
 }
+
+function deactiveMenuSongs(){
+    Array.from(document.querySelectorAll(".menu-songs ul .song-list")).forEach(e => {
+        e.style.backgroundColor = "var(--color2)";
+        e.style.color = "var(--color)";
+    })
+}
 var songs;
 // Xhr requestion to getting all the songs stored on songs.json file
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "./songs.json", true);
+xhr.open("GET", "./jsonFiles/songs.json", true);
 xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
         songs = JSON.parse(xhr.responseText);
@@ -195,42 +184,42 @@ xhr.onreadystatechange = function () {
                         <li class="song-list list" onmouseover="this.classList.add('hover');" onmouseout="this.classList.remove('hover');">
                             <div>
                                 <span>${temp}</span>
-                                <img src="${element.poster}" class="" alt="">
-                                <h5 class="title">${element.songName}</h5>
+                                <img src="" id="img-${element.id}" class="" alt="">
+                                <h5 class="title" id="title-${element.id}"></h5>
                             </div>
-                            <i class="bi playbutton bi-play-circle-fill" id="${element.id}"></i>
+                            <i class="bi playbutton bi-play-circle-fill" data-custom-value="${i}" id="${element.id}"></i>
                         </li>
-                `;
+                        `;
             } else {
                 document.querySelector(".items ul").innerHTML += `
-                    <li class="song-list list">
+                        <li class="song-list list">
                         <div class="song">
-                            <img src="${element.poster}" alt="">
-                            <i class="bi playbutton bi-play-circle-fill" id="${i + 1}"></i>
+                        <img src="" id="img-${element.id}" class="" alt="">
+                        <i class="bi playbutton bi-play-circle-fill" data-custom-value="${i}" id="${element.id}"></i>
                         </div>
-                        <h5 class="title">${element.songName}</h5>
-                    </li>
-                `
+                        <h5 class="title" id="title-${element.id}"></h5>
+                        </li>
+                        `
+
             }
         });
+        for (let index = 0; index < songs.length; index++) {
+            getImageThumbnail(`../audio/${index + 1}.mp3`, document.getElementById(`img-${index + 1}`), document.getElementById(`title-${index + 1}`))
+        }
     }
 
 
 
-    
+
 
     // getting all play button and using this we play songs...
     Array.from(document.getElementsByClassName("bi-play-circle-fill")).forEach((e, i) => {
         e.addEventListener("click", () => {
             pauseAllBtns();
-            document.querySelector("footer img").src = "./img/" + e.getAttribute("id") + ".jpg";
-            changeMasterPlay(e.getAttribute("id") - 1);
+            changeMasterPlay(e.dataset.customValue, e.getAttribute("id"));
             if (music.paused) {
                 if (e.getAttribute("id") <= 7) {
-                    Array.from(document.querySelectorAll(".menu-songs ul .song-list")).forEach(e => {
-                        e.style.backgroundColor = "var(--color2)";
-                        e.style.color = "var(--color)";
-                    })
+                    deactiveMenuSongs();
                     document.querySelectorAll(".menu-songs ul .song-list")[e.getAttribute("id") - 1].style.backgroundColor = "var(--color)";
                     document.querySelectorAll(".menu-songs ul .song-list")[e.getAttribute("id") - 1].style.color = "var(--textcolor1)";
                 }
@@ -328,43 +317,13 @@ document.querySelector(".dark-btn").addEventListener("click", () => {
     i++;
 })
 
-// function to go previous song
-function goPrev(e) {
-    let songid = document.querySelector(".play-and-pause").getAttribute("id").split('-')[1];
-    if (parseInt(songid) <= 1) {
-        customAlertShow("This is the first song that you are listening!");
-    } else {
-        songid = parseInt(songid) - 2;
-        changeMasterPlay(songid);
-        music.src = `./audio/${songid + 1}.mp3`;
-        buffering();
-        music.play();
-        document.querySelector(".icons .play-and-pause").classList.add("bi-pause-fill")
-        document.querySelector(".icons .play-and-pause").classList.remove("bi-play-fill")
-    }
-}
 
-// function to go next song
-function goNext(e) {
-    let songid = document.querySelector(".play-and-pause").getAttribute("id").split('-')[1];
-    songid = parseInt(songid);
-    if (songid >= songs.length) {
-        customAlertShow("This is the last song that you are listening!")
-    } else {
-        changeMasterPlay(songid);
-        console.log();
-        music.src = `./audio/${songid + 1}.mp3`;
-        buffering();
-        music.play();
-        document.querySelector(".icons .play-and-pause").classList.add("bi-pause-fill")
-        document.querySelector(".icons .play-and-pause").classList.remove("bi-play-fill")
-    }
-}
 
 // when we click on masterplay play icon then ...
 function playmasterplay(e) {
     if (music.paused) {
         if (progressBar.value == 0) {
+            console.log(e.getAttribute("id").split('-')[1]);
             music.src = "./audio/" + e.getAttribute("id").split('-')[1] + ".mp3";
             buffering();
             music.play();
